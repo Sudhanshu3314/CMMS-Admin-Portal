ab387042f443bec3cffa96c0b0b07458a2e579df
Sudhanshu Barnwal <83909429+Sudhanshu3314@users.noreply.github.com>
2025-12-19 17:00:12 +0530

    Update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://cmms-backends.vercel.app",
+    // baseURL: "https://cmms-backends.vercel.app",
+    baseURL: "http://localhost:8080",
     // withCredentials: true
 });
 
